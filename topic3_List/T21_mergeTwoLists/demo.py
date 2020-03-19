afdf0405a4a784783b2e03819c846c88a40d101f
km <958747457@qq.com>
2020-03-19 14:04:38 +0800

    21. 合并两个有序链表

--- topic3_List/T21_mergeTwoLists/demo.py
+++ topic3_List/T21_mergeTwoLists/demo.py
@@ -11,16 +11,40 @@
 import sys
 sys.path.append("..") # 这句是为了导入_config
 
-from T0_ListDifine.tools import arr2List,print_list
+from T0_ListDifine.tools import arr2List,print_list,ListNode
 
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
-        pass
+        head = ListNode(0)
+        h = head
 
+        while l1 and l2:
+            if l1.val >= l2.val:
+                h.next = l2
+                l2 = l2.next
+            else:
+                h.next = l1
+                l1 = l1.next
+            h = h.next
+        
+        if l1:
+            h.next = l1
+            
+
+        if l2:
+            h.next = l2 
+        return head.next
 
 if __name__ == "__main__":
-    pass
+    l1 = arr2List([1,2,4,5,6,8,9])
+    l2 = arr2List([1,3,4])
+    print_list(l1)
+    print_list(l2)
+    solution = Solution()
+    res = solution.mergeTwoLists(l1,l2)
+
+    print_list(res)
     
 
 
